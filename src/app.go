7e986daa2a923f9b7ed8c10b01e0c6f57955edb7
王純 <brightzamber@gmail.com>
2019-04-09 17:29:03 +0900

    add mongodb

--- src/app.go
+++ src/app.go
@@ -1,21 +1,47 @@
 package main
 
 import (
+	"log"
 	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/globalsign/mgo"
+	"github.com/globalsign/mgo/bson"
 )
 
+type Person struct {
+	ID   bson.ObjectId `bson:"_id"`
+	Name string        `bson:"name"`
+	Age  int           `bson:"age"`
+}
+
 func main() {
+	session, _ := mgo.Dial("mongodb://root:example@localhost:27017")
+	defer session.Close()
+	db := session.DB("test")
+
+	ritsu := &Person{
+		ID:   bson.NewObjectId(),
+		Name: "田井中律",
+		Age:  17,
+	}
+	col := db.C("persons")
+	if err := col.Insert(ritsu); err != nil {
+		log.Fatalln(err)
+	}
+
+	p := new(Person)
+	query := db.C("persons").Find(bson.M{})
+	query.One(&p)
+
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
+		c.JSON(200, p)
 	})
 	port := os.Getenv("PORT")
 	if len(port) == 0 {
-		port = "3000"
+		port = "5000"
 	}
 	r.Run(":" + port)
+
 }
