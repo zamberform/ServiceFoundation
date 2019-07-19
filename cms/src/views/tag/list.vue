775d7e799b0a52260e2ddf7a02b0d44b071f92b7
zamberform <brightzamber@gmail.com>
2019-07-19 19:59:45 +0900

    Update list.vue

--- cms/src/views/tag/list.vue
+++ cms/src/views/tag/list.vue
@@ -30,22 +30,22 @@
           </el-table-column>
           <el-table-column label="Pageviews" width="110" align="center">
             <template slot-scope="scope">
-            {{ scope.row.pageviews }}
+              {{ scope.row.pageviews }}
             </template>
           </el-table-column>
-            <el-table-column class-name="status-col" label="Status" width="110" align="center">
-              <template slot-scope="scope">
-                <el-tag :type="scope.row.status | statusFilter">{{ scope.row.status }}</el-tag>
-              </template>
-            </el-table-column>
-            <el-table-column align="center" prop="created_at" label="Display_time" width="200">
-              <template slot-scope="scope">
-                <i class="el-icon-time" />
-                  <span>{{ scope.row.display_time }}</span>
-              </template>
-            </el-table-column>
+          <el-table-column class-name="status-col" label="Status" width="110" align="center">
+            <template slot-scope="scope">
+              <el-tag :type="scope.row.status | statusFilter">{{ scope.row.status }}</el-tag>
+            </template>
+          </el-table-column>
+          <el-table-column align="center" prop="created_at" label="Display_time" width="200">
+            <template slot-scope="scope">
+              <i class="el-icon-time" />
+              <span>{{ scope.row.display_time }}</span>
+            </template>
+          </el-table-column>
         </el-table>
-      </el-form-item>   
+      </el-form-item>
     </el-form>
   </div>
 </template>
